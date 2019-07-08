f4cd529f29463e5686f4131ffcabc7bf23c352af
Your Name <you@example.com>
2019-07-08 15:19:01 +0530

    String types in GoLang

---
+++ string_types.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "Hello World"
+	s1 := `Hello 
+		World`
+	fmt.Println(s)
+	fmt.Println(s1)
+	
+	bs := []byte(s)
+	bs1 := []byte(s1)
+	
+	fmt.Println(bs)
+	fmt.Printf("%T\n",bs)
+	fmt.Println(bs1)
+	fmt.Printf("%T\n",bs1)
+}
