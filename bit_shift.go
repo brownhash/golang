0054b647e6dd615896ed462a998db615947802cc
Your Name <you@example.com>
2019-07-08 16:19:17 +0530

    Bit Shifting in GoLang

--- bit_shift.go
+++ bit_shift.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	x := 4
+	x := 2
 	fmt.Printf("%d\t\t%b\n",x,x)
 	
 	y := x << 1
 	fmt.Printf("%d\t\t%b\n",y,y)
 
-	z := y << 1
+	z := y << 2
         fmt.Printf("%d\t\t%b\n",z,z)
 }
