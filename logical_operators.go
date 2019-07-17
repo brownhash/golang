680dc473123996dfec8f60efb111cda0e7ea8035
Your Name <you@example.com>
2019-07-17 15:05:11 +0530

    Using logical operators in GoLang

---
+++ logical_operators.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("true || flse --> ",(true || false))
+	fmt.Println("true && false --> ",(true && false))
+	fmt.Println("false || true --> ",(false || true))
+	fmt.Println("false && true --> ",(false && true))
+}
