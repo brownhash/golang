a44c9f9c69468e9aedf5de51e8493c1e766bbbfc
Harshit Sharma <you@example.com>
2019-07-23 17:33:00 +0530

    Multi dimension slices in GoLang

--- go_server/server.go
+++ go_server/server.go
@@ -3,11 +3,30 @@ package main
 import (
     "log"
     "net/http"
+    //"os/exec"
+    //"os"
+    //"fmt"
 )
 
-func main() {
-	var http_path = "/Users/username/golang/go_server/http/"
-	http.Handle("/", http.FileServer(http.Dir(http_path)))
+func redirect(w http.ResponseWriter, r *http.Request) {
 
-	log.Fatal(http.ListenAndServe(":80", nil))
+    http.Redirect(w, r, "http://localhost:5000", 301)
+}
+
+func main() {
+
+	//cmd := exec.Command("/usr/bin/python", "/Users/harshitsharma/Documents/golang/src/golang/go_server/backend/backend_server.py &&")
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	//err := cmd.Run()
+	//if err != nil{ 
+	//	fmt.Println("Backend server executed")
+	//}
+
+	http.HandleFunc("/", redirect)
+	err_http := http.ListenAndServe(":8080", nil)
+
+	if err_http != nil {
+		log.Fatal("ListenAndServe: ", err_http)
+	}
 }
